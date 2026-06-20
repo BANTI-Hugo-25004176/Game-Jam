33adaa48410e5731e009200744861cc8b1da4fa6
MORSILLI-Timeo-25002755 <timeo.morsilli@etu.univ-amu.fr>
2026-06-20 15:31:16 +0200

    feat(Actor): add gun gameplay

---
+++ scenes/bullet/bullet.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const SPEED = 1000.0
+var pos: Vector2
+var rota: float
+var dir: float
+
+func _ready():
+	global_position = pos
+	global_rotation = rota
+	
+	velocity = Vector2(SPEED, 0).rotated(dir)
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
