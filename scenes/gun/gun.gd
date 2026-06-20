f1460603a708102c97c4c9f3177ab9cd3f9b4713
Vladislav DUMONT <vladislav.dumont@yahoo.com>
2026-06-21 00:41:45 +0200

    feat(gun) : added gun orientation and reload system

--- scenes/gun/gun.gd
+++ scenes/gun/gun.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
+
 var bullet_path = preload("res://scenes/bullet/bullet.tscn")
+var ammo = 30
+var reloading = false
+@onready var timer: Timer = $Timer
 
 func _physics_process(_delta: float) -> void:
 	
@@ -8,12 +12,31 @@ func _physics_process(_delta: float) -> void:
 		fire()
 	
 	look_at(get_global_mouse_position())
+	
+	var mouse_pos = get_global_mouse_position()
+	var direction = (mouse_pos - global_position).normalized()
+
+	if direction.x < 0:
+		$Gun.flip_v = true   
+	else:
+		$Gun.flip_v = false 
 
 func fire():
-	var bullet = bullet_path.instantiate()
+	if !reloading:
+		
+		var bullet = bullet_path.instantiate()
 	
-	bullet.pos = $Marker2D.global_position
-	bullet.rota = global_rotation
-	bullet.dir = global_rotation
+		bullet.pos = $Marker2D.global_position
+		bullet.rota = global_rotation
+		bullet.dir = global_rotation
 	
-	get_tree().current_scene.add_child(bullet)
+		get_tree().current_scene.add_child(bullet)
+		ammo -= 1
+		if ammo == 0:
+			reloading = true
+			timer.start()
+
+
+func _on_timer_timeout() -> void:
+	ammo = 30
+	reloading = false
