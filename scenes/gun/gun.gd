4e236ae8e9f368a1b10545d130d15c0a6f88d632
Androtim <timothee.beghin@etu.univ-amu.fr>
2026-06-21 01:05:15 +0200

    feat(gamepad): visée au stick droit + tir à la gâchette (R2/R1)

--- scenes/gun/gun.gd
+++ scenes/gun/gun.gd
@@ -7,25 +7,39 @@ var reloading = false
 var cadence_de_tir : float = 7.0
 var delai_entre_tirs : float = 1.0 / cadence_de_tir # Donne 0.2 seconde
 var temps_depuis_dernier_tir : float = 0.0
+
+## Zone morte du stick droit pour la visée manette.
+const AIM_DEADZONE : float = 0.3
+
 @onready var timer: Timer = $Timer
 
 func _physics_process(delta: float) -> void:
-	
+
 	temps_depuis_dernier_tir += delta
-	
+
 	if Input.is_action_pressed("shoot") and temps_depuis_dernier_tir >= delai_entre_tirs:
 		fire()
 		temps_depuis_dernier_tir = 0.0
-	
-	look_at(get_global_mouse_position())
-	
-	var mouse_pos = get_global_mouse_position()
-	var direction = (mouse_pos - global_position).normalized()
 
-	if direction.x < 0:
-		$Gun.flip_v = true   
+	_viser()
+
+## Oriente l'arme : stick droit en priorité (manette), sinon la souris.
+func _viser() -> void:
+	var stick := Vector2(
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y))
+
+	var direction : Vector2
+	if stick.length() >= AIM_DEADZONE:
+		# Visée à la manette : on pointe dans le sens du stick droit.
+		rotation = stick.angle()
+		direction = stick
 	else:
-		$Gun.flip_v = false 
+		# Repli souris (clavier/souris).
+		look_at(get_global_mouse_position())
+		direction = get_global_mouse_position() - global_position
+
+	$Gun.flip_v = direction.x < 0
 
 func fire():
 	if !reloading:
